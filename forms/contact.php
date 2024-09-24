0a5cd6cd7b302ab0002ff2a16b0f8b61faee0e74
Rahan Reza Mohiyyaddin <rayanraazz1@gmail.com>
2024-09-24 18:16:25 +0530

    Updated contact form to send messages to Google Sheets

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,32 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// The URL of your deployed Google Apps Script
+$google_script_url = 'https://script.google.com/macros/s/AKfycbzoWgq36JKt8Cjy-LTKH-QS3YhBITM33B-bwlfD8guluusZcp-GI2Oc1jXJu6i5yRMBRg/exec ';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rayanraazz1@gmail.com';
+$data = array(
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+    'subject' => $_POST['subject'],
+    'message' => $_POST['message']
+);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Use cURL to send data to Google Apps Script
+$options = array(
+    CURLOPT_URL => $google_script_url,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => $data,
+    CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded')
+);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$response = curl_exec($ch);
+curl_close($ch);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// You can check the response if needed
+if ($response) {
+    echo 'Message sent successfully!';
+} else {
+    echo 'Error sending message.';
+}
 ?>
